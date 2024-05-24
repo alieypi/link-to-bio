217a66112d8491fc526ecb27bffcdad70b46a4f7
alieypi <eypidev@gmail.com>
2024-05-24 15:46:38 +0330

    deleted duration-300 class from MainButton component

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -17,7 +17,7 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <a :href="link" class="flex flex-row justify-between items-center rounded-[20px] h-[70px] w-full duration-300 px-[20px] py-[18px]">
+  <a :href="link" class="flex flex-row justify-between items-center rounded-[20px] h-[70px] w-full px-[20px] py-[18px]">
     <div class="flex flex-row gap-[10px] items-center">
       <Icon :icon width="22" height="22" />
       <span class="text-[18px] font-bold">{{ title }}</span>
